4e4968956f11daddf5ccb1669c366f10f93e7310
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-03-17 23:18:21 +0100

    feat(incubator): bootloader

--- hosts/incubator/configuration.nix
+++ hosts/incubator/configuration.nix
@@ -44,6 +44,19 @@
     ];
   };
 
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    loader = {
+      systemd-boot = {
+        enable = true;
+        timeout = 0;
+      };
+
+      efi.canTouchEfiVariables = true;
+    };
+  }
+
   users.motd = with config; ''
     Incubator; Faye's Homeserver
 
